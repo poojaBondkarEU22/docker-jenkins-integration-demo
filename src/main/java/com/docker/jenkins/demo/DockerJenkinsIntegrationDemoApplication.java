c30e8bc3bd8c1433e0cd4d4b03e1c0cc544ca3a5
Pooja Bondkar <p.bondkar@wealthpilot.de>
2023-08-03 22:08:04 +0200

    first commit

---
+++ src/main/java/com/docker/jenkins/demo/DockerJenkinsIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.docker.jenkins.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker/jenkins/demo/DockerJenkinsIntegrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.jenkins.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
